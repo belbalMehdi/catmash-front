93df8c99fa823048eacdf2947ec316e24e30f50b
Mehdi <belbalmehdi@gmail.com>
2019-10-01 18:44:58 +0200

    Configuration des routes de l'application: rate et ranking. La route par défaut est ranking Les routes non configurer pointent vers le composant notFound

--- catmash-front/src/app/app-routing.module.ts
+++ catmash-front/src/app/app-routing.module.ts
@@ -4,7 +4,25 @@ import { RateComponent } from './rate/rate.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rate',
+    component: RateComponent
+  },
+  {
+    path: 'ranking',
+    component: RankingComponent
+  },
+  {
+    path: '',
+    redirectTo: 'ranking',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
